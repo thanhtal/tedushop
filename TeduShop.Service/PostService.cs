12d797bda9de9dde693a45ba627553659a9926f7
DESKTOP-LJN95TQ\banggia <nuocmatsaobang9999@gmail.com>
2017-04-01 02:36:26 +0700

    change project

--- TeduShop.Service/PostService.cs
+++ TeduShop.Service/PostService.cs
@@ -18,6 +18,7 @@ namespace TeduShop.Service
         IEnumerable<Post> GetAll();
 
         IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+        IEnumerable<Post> GetAllByCategory(int categoryId, int page, int pageSize, out int totalRow);
 
         Post GetById(int id);
 
@@ -52,10 +53,15 @@ namespace TeduShop.Service
             return _postRepository.GetAll(new string[] { "PostCategory" });
         }
 
+        public IEnumerable<Post> GetAllByCategory(int categoryId, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status && x.CategoryID == categoryId, out totalRow, page, pageSize,new string[] { "PostCategory "});
+        }
+
         public IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow)
         {
             //TODO: Select all post by tag
-            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+            return _postRepository.GetAllByTag(tag, page, pageSize, out totalRow);
 
         }
 
