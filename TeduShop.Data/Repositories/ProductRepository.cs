ed4b8ce73aa1b55c2c1fe0283aa632e3d162fbb5
DESKTOP-LJN95TQ\banggia <nuocmatsaobang9999@gmail.com>
2017-04-01 00:43:59 +0700

    fixbug for repository and add post service

--- TeduShop.Data/Repositories/ProductRepository.cs
+++ TeduShop.Data/Repositories/ProductRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
 {
-    class ProductRepository
+    public interface IProductRepository : IRepository<Product>
     {
+    }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
     }
 }
