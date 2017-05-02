040e4501d41a12f83ba1d08b30afd31807bc0de3
DESKTOP-LJN95TQ\banggia <nuocmatsaobang9999@gmail.com>
2017-05-02 10:05:01 +0700

    Automapper 5x  for web

---
+++ TeduShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using TeduShop.Model.Models;
+using TeduShop.Web.Models;
+
+namespace TeduShop.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            
+
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Post, PostViewModel>();
+                cfg.CreateMap<PostCategory, PostCategoryViewModel>();
+                cfg.CreateMap<Tag, TagViewModel>();
+            });
+        }
+    }
+}
