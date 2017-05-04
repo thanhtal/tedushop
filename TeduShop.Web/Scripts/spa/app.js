6d3d4cf1862d9f9039daf479a73ce8735e422a1e
DESKTOP-LJN95TQ\banggia <nuocmatsaobang9999@gmail.com>
2017-05-04 22:17:21 +0700

    service in angularjs

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+var myApp = angular.module('myModule', []);
+
+myApp.controller("schoolController", schoolController);
+myApp.service('Validator', Validator);
+
+schoolController.$inject = ['$scope', 'Validator'];
+
+function schoolController($scope, Validator) {
+
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
+    $scope.num = 1;
+}
+
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return 'This is even';
+        }
+        else
+            return 'This is odd';
+    }
+
+}
+
